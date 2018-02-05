dc7d428390344f5cd392ff9060f57e1bfb6e4c3f
quoc phan <phquoc25@gmail.com>
2018-02-05 23:51:34 +0700

    angular study:  Using HttpClient to request data from the server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,20 +4,53 @@ import { Hero } from './hero';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('Heroes get fetched successfully from server.');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log('Heroes get fetched successfully from server.')),
+      catchError(this.handleError('getHeroes', []))
+    );
   }
 
-  getHero(id): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    const heroFound = HEROES.find((hero) => hero.id === id);
-    return of(heroFound);
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      // TODO: better job of transforming error for user
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    }
   }
 }
