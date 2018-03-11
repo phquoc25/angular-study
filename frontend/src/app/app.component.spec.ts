4628a9f88aefc4608f8666151cfa8e8685bd4aa7
quoc phan <phquoc25@gmail.com>
2018-03-11 21:31:55 +0700

    Test routerLink in AppComponent

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { Component } from '@angular/core'
+import { By } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterLinkDirectiveStub } from './router-link-directive-stub';
@@ -11,6 +12,10 @@ class AppMessagesStubComponent {}
 class RouterOutletStubComponent { }
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+  let linkDes;
+  let routerLinks;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -20,22 +25,42 @@ describe('AppComponent', () => {
         RouterLinkDirectiveStub
       ],
     }).compileComponents();
+    
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+
+    fixture.detectChanges(); // trigger initial data binding
+
+    // find DebugElements with an attached RouterLinkDirectiveStub
+    linkDes = fixture.debugElement.queryAll(By.directive(RouterLinkDirectiveStub));
+
+    // get attached link directive instances
+    // using each DebugElement's injector
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it(`should have as title 'Tour of Heroes'`, async(() => {
+    expect(app.title).toEqual('Tour of Heroes');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  it('can get RouterLinks from template', () => {
+    expect(routerLinks.length).toBe(2, 'should have 3 routerLinks');
+    expect(routerLinks[0].linkParams).toBe('/dashboard');
+    expect(routerLinks[1].linkParams).toBe('/heroes');
+  });
+
+  it('can click heroes link in template', () => {
+    const heroesLinkDe = linkDes[1];   // heroes link DebugElement
+    const heroesLink = routerLinks[1]; // heroes link directive
+
+    expect(heroesLink.navigatedTo).toBeNull('should not have navigated yet');
+
+    heroesLinkDe.triggerEventHandler('click', null);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+    expect(heroesLink.navigatedTo).toBe('/heroes');
+  });
 });
  
